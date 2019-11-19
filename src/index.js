00dc28b2bacf353f73bd91cb53f8dcc35991c6be
Aleksei Ulianov <al.ulianov@gmail.com>
2019-11-19 12:14:17 +0400

    Add align to plugin

--- src/index.js
+++ src/index.js
@@ -42,7 +42,10 @@ class Paragraph {
 
     this._CSS = {
       block: this.api.styles.block,
-      wrapper: 'ce-paragraph'
+      settingsButton: this.api.styles.settingsButton,
+      settingsButtonActive: this.api.styles.settingsButtonActive,
+      wrapper: 'ce-paragraph',
+      align: 'left',
     };
     this.onKeyUp = this.onKeyUp.bind(this);
 
@@ -53,6 +56,11 @@ class Paragraph {
     this._placeholder = config.placeholder ? config.placeholder : Paragraph.DEFAULT_PLACEHOLDER;
     this._data = {};
     this._element = this.drawView();
+    /**
+     * List of settings buttons
+     * @type {HTMLElement[]}
+     */
+    this.settingsButtons = [];
 
     this.data = data;
   }
@@ -86,6 +94,10 @@ class Paragraph {
     div.classList.add(this._CSS.wrapper, this._CSS.block);
     div.contentEditable = true;
     div.dataset.placeholder = this._placeholder;
+    /**
+     * Add styles align
+     */
+    div.style.textAlign = this._CSS.align;
 
     div.addEventListener('keyup', this.onKeyUp);
 
@@ -102,6 +114,50 @@ class Paragraph {
   }
 
   /**
+   * Create Block's settings block
+   *
+   * @return {HTMLElement}
+   */
+  renderSettings() {
+    let holder = document.createElement('DIV');
+    /** Add align styles */
+    this.align.forEach( item => {
+      let selectTypeButton = document.createElement('SPAN');
+
+      selectTypeButton.classList.add(this._CSS.settingsButton);
+
+      /**
+       * Add SVG icon
+       */
+      selectTypeButton.innerHTML = item.svg;
+
+      /**
+       * Save align to its button
+       */
+      selectTypeButton.dataset.align = item.align;
+
+      /**
+       * Set up click handler
+       */
+      selectTypeButton.addEventListener('click', () => {
+        this.setAlign(item.align);
+      });
+
+      /**
+       * Append settings button to holder
+       */
+      holder.appendChild(selectTypeButton);
+
+      /**
+       * Save settings buttons
+       */
+      this.settingsButtons.push(selectTypeButton);
+    });
+
+    return holder;
+  }
+
+  /**
    * Method that specified how to merge two Text blocks.
    * Called by Editor.js by backspace at the beginning of the Block
    * @param {ParagraphData} data
@@ -113,6 +169,23 @@ class Paragraph {
     };
 
     this.data = newData;
+  }
+
+  /**
+   * Callback for Block's settings buttons
+   * @param align
+   */
+  setAlign(align) {
+    this.data = {
+      align: align,
+      text: this.data.text
+    };
+    /**
+     * Highlight button by selected align
+     */
+    this.settingsButtons.forEach(button => {
+      if (button.dataset.align) button.classList.toggle(this._CSS.settingsButtonActive, button.dataset.align === align);
+    });
   }
 
   /**
@@ -139,7 +212,8 @@ class Paragraph {
    */
   save(toolsContent) {
     return {
-      text: toolsContent.innerHTML
+      text: toolsContent.innerHTML,
+      align: this.currentAlign.align
     };
   }
 
@@ -150,7 +224,8 @@ class Paragraph {
    */
   onPaste(event) {
     const data = {
-      text: event.detail.data.innerHTML
+      text: event.detail.data.innerHTML,
+      align: 'left',
     };
 
     this.data = data;
@@ -173,7 +248,8 @@ class Paragraph {
     return {
       text: {
         br: true,
-      }
+      },
+      align: {}
     };
   }
 
@@ -186,8 +262,55 @@ class Paragraph {
     let text = this._element.innerHTML;
 
     this._data.text = text;
+    this._data.align = this.currentAlign.align;
 
     return this._data;
+  }
+
+  /**
+   * Return default align
+   * @returns {align}
+   */
+  get defaultAlign() {
+    /**
+     * Use left as default align
+     */
+    return this.align[0];
+  }
+
+  /**
+   * Get current align
+   * @return {align}
+   */
+  get currentAlign() {
+    let align = this.align.find(item => item.align === this._data.align);
+
+    if (!align) {
+      align = this.defaultAlign;
+    }
+
+    return align;
+  }
+
+  /**
+   * Available aligns
+   * @return {align[]}
+   */
+  get align() {
+    return [
+      {
+        align: 'left',
+        svg: '<svg width="18" height="14" aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M12.83 352h262.34A12.82 12.82 0 0 0 288 339.17v-38.34A12.82 12.82 0 0 0 275.17 288H12.83A12.82 12.82 0 0 0 0 300.83v38.34A12.82 12.82 0 0 0 12.83 352zm0-256h262.34A12.82 12.82 0 0 0 288 83.17V44.83A12.82 12.82 0 0 0 275.17 32H12.83A12.82 12.82 0 0 0 0 44.83v38.34A12.82 12.82 0 0 0 12.83 96zM432 160H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm0 256H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16z"></path></svg>'
+      },
+      {
+        align: 'center',
+        svg: '<svg width="18" height="14" aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M432 160H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm0 256H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zM108.1 96h231.81A12.09 12.09 0 0 0 352 83.9V44.09A12.09 12.09 0 0 0 339.91 32H108.1A12.09 12.09 0 0 0 96 44.09V83.9A12.1 12.1 0 0 0 108.1 96zm231.81 256A12.09 12.09 0 0 0 352 339.9v-39.81A12.09 12.09 0 0 0 339.91 288H108.1A12.09 12.09 0 0 0 96 300.09v39.81a12.1 12.1 0 0 0 12.1 12.1z"></path></svg>'
+      },
+      {
+        align: 'right',
+        svg: '<svg width="18" height="14" aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 224h416a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16zm416 192H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm3.17-384H172.83A12.82 12.82 0 0 0 160 44.83v38.34A12.82 12.82 0 0 0 172.83 96h262.34A12.82 12.82 0 0 0 448 83.17V44.83A12.82 12.82 0 0 0 435.17 32zm0 256H172.83A12.82 12.82 0 0 0 160 300.83v38.34A12.82 12.82 0 0 0 172.83 352h262.34A12.82 12.82 0 0 0 448 339.17v-38.34A12.82 12.82 0 0 0 435.17 288z"></path></svg>'
+      },
+    ];
   }
 
   /**
@@ -199,9 +322,23 @@ class Paragraph {
    * @private
    */
   set data(data) {
-    this._data = data || {};
+    /**
+     * If align is set and block in DOM
+     * then replace it to a new block
+     */
+    if (data.align !== undefined) {
+      /**
+       * Save Block's align
+       */
+      this._element.style.textAlign = data.align;
+    }
 
-    this._element.innerHTML = this._data.text || '';
+    /**
+     * If data.text was passed then update block's content
+     */
+    if (data.text !== undefined) {
+      this._element.innerHTML = data.text || '';
+    }
   }
 
   /**
@@ -213,6 +350,18 @@ class Paragraph {
   static get pasteConfig() {
     return {
       tags: [ 'P' ]
+    };
+  }
+
+  /**
+   * Icon and title for displaying at the Toolbox
+   *
+   * @return {{icon: string, title: string}}
+   */
+  static get toolbox() {
+    return {
+      icon: '<svg aria-hidden="true" focusable="false" role="img" viewBox="0.2 -0.3 9 11.4" width="12" height="14"><path d="M0 2.77V.92A1 1 0 01.2.28C.35.1.56 0 .83 0h7.66c.28.01.48.1.63.28.14.17.21.38.21.64v1.85c0 .26-.08.48-.23.66-.15.17-.37.26-.66.26-.28 0-.5-.09-.64-.26a1 1 0 01-.21-.66V1.69H5.6v7.58h.5c.25 0 .45.08.6.23.17.16.25.35.25.6s-.08.45-.24.6a.87.87 0 01-.62.22H3.21a.87.87 0 01-.61-.22.78.78 0 01-.24-.6c0-.25.08-.44.24-.6a.85.85 0 01.61-.23h.5V1.7H1.73v1.08c0 .26-.08.48-.23.66-.15.17-.37.26-.66.26-.28 0-.5-.09-.64-.26A1 1 0 010 2.77z"/></svg>',
+      title: 'Text'
     };
   }
 }
